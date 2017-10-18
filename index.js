c50acba42ba4d97ed9dd627a79dc0550cf3bd417
Chris <chriswc14@yahoo.com>
2017-10-18 18:35:45 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import discord from 'discord.io';
+
+var client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+        message.reply('pong');
+    }
+});
+
+client.login('your token');
