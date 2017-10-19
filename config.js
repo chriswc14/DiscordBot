bc611b99c7d936e648ee79675e480e66c62a19b7
Banrion121 <lrjenkins121@gmail.com>
2017-10-19 13:32:51 -0500

    Fixed SayTon's name

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ var config = {
         key: 'MzcwMzY1MjQ0Mzg1NjU2ODUy.DMpwRw.B0oK1fvkQG8sOJLKVokB2wjKZ28'
     },
     SayTon = {
-        name: 'SuperSayTon',
+        name: 'SayTon',
         key: 'MzcwMzU4MzMzNzk1NzI5NDI5.DMl6qA.qh1pIxKaWWj-v_LKZYHsLWaMsVI'
     }
 }
