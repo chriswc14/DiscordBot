baa9506b2b180ba5abe0b535c2790d8843f38d16
Banrion121 <lrjenkins121@gmail.com>
2017-10-19 12:31:00 -0500

    Added config

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {
+    SuperSayTon = {
+        name: 'SuperSayTon',
+        key: 'MzcwMzY1MjQ0Mzg1NjU2ODUy.DMpwRw.B0oK1fvkQG8sOJLKVokB2wjKZ28'
+    },
+    SayTon = {
+        name: 'SuperSayTon',
+        key: 'MzcwMzU4MzMzNzk1NzI5NDI5.DMl6qA.qh1pIxKaWWj-v_LKZYHsLWaMsVI'
+    }
+}
