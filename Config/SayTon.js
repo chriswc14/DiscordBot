fa89df5bcdb17fc75eb7b371e737934fe46d3024
Chris <chriswc14@yahoo.com>
2017-10-29 14:54:04 -0500

    Missed something.

--- Config/SayTon.js
+++ Config/SayTon.js
@@ -1,4 +1,5 @@
 var config = {
     "name": 'SayTon',
-    "key": 'MzcwMzU4MzMzNzk1NzI5NDI5.DMl6qA.qh1pIxKaWWj-v_LKZYHsLWaMsVI'
+    "key": 'MzcwMzU4MzMzNzk1NzI5NDI5.DMl6qA.qh1pIxKaWWj-v_LKZYHsLWaMsVI',
+    "userId": '370358333795729429'
 }
--- Config/SuperSayTon.js
+++ Config/SuperSayTon.js
@@ -1,5 +1,5 @@
 var config = {
-    name: 'SuperSayTon',
-    key: 'MzcwMzY1MjQ0Mzg1NjU2ODUy.DMpwRw.B0oK1fvkQG8sOJLKVokB2wjKZ28',
-    userId: '370365244385656852'
+    "name": 'SuperSayTon',
+    "key": 'MzcwMzY1MjQ0Mzg1NjU2ODUy.DMpwRw.B0oK1fvkQG8sOJLKVokB2wjKZ28',
+    "userId": '370365244385656852'
 }
